7818fc419edc0aeebd95e27de8335648e584e1cd
danieldelahoz <86014542+danieldelahoz@users.noreply.github.com>
2021-12-16 21:02:44 -0500

    Update WasteGas.sol

--- contracts/WasteGas.sol
+++ contracts/WasteGas.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.7;
 
 // Flashbots currently enforces a gas used floor. As of August 2021, that value is 42,000 gas. Any transaction that consumes less than this floor is rejected by relay
 // This contract enables testing Flashbots with a single, simple transaction that will consume transaction's gas limit, ensuring the bundle won't be rejected
-// See: https://docs.flashbots.net/flashbots-auction/searchers/advanced/bundle-pricing
 
 contract WasteGas {
     event Waste(address sender, uint256 gas);
